e03626ba47074cd11f7c6c0755791186a07e7848
Leon Coto <leon.coto@mcsaatchi.com>
2013-08-14 15:25:03 +0100

    meta-carnaby setup function

--- setup.sh
+++ setup.sh
@@ -1,14 +1,55 @@
 #!/usr/bin/env bash
+
+# Bring the code
+echo
+echo 'Cloning git repos...'
+echo
+rm -rf grunt-carnaby grunt-init-carnaby &&
 git clone git@github.com:elgrancalavera/grunt-carnaby.git &&
 git clone git@github.com:elgrancalavera/grunt-init-carnaby.git &&
+
+# Symlik the init template
+mkdir -p ~/.grunt-init/
+rm -f ~/.grunt-init/carnaby
+from=$(pwd)'/grunt-init-carnaby'
+to=~/.grunt-init
+to=$to'/carnaby'
+echo
+echo 'Symlinking grunt-init-carnaby template...'
+echo 'From: ' $from
+echo 'To:   ' $to
+echo
+ln -s $from $to
+ls -laf ~/.grunt-init
+
+# Symlink grunt-carnaby gruntplugin
+mkdir -p grunt-init-carnaby/node_modules
+from=$(pwd)'/grunt-carnaby'
+to=$(pwd)'/grunt-init-carnaby/node_modules'
+echo
+echo 'Symlinking grunt-carnaby gruntplugin...'
+echo 'From: ' $from
+echo 'To:   ' $to
+echo
+ln -s $from $to
+ls -laf grunt-init-carnaby/node_modules
+
+# Installing grunt-carnaby
+echo
+echo 'Insalling grunt-carnaby plugin...'
+echo
 cd grunt-carnaby &&
-grunt &&
 npm install &&
+grunt &&
+
+# Installing grunt-init-carnaby
+echo
+echo 'Installing grunt-init-carnaby template...'
+echo
 cd ../grunt-init-carnaby &&
 npm install &&
-cd node_modules &&
-ln -s ./../../grunt-carnaby/ grunt-carnaby &&
-cd ..
 grunt &&
 
-
+echo
+echo 'Done.'
+echo
