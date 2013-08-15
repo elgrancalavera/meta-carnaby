3ac86e2e3a10b665d508ef8e1933d8b7a4f7d74f
Leon Coto <leon.coto@mcsaatchi.com>
2013-08-15 18:59:40 +0100

    Install or not using a flag.

--- test.sh
+++ test.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 rm -rf a-carnaby-project && mkdir a-carnaby-project &&
-cd a-carnaby-project && grunt-init carnaby &&
-subl a-carnaby-project.sublime-project &&
-npm install && bower install && grunt
+cd a-carnaby-project && grunt-init carnaby -d -v &&
+subl a-carnaby-project.sublime-project
+
+while getopts ":i" opt ; do
+  case $opt in
+    i )
+    install=true
+    ;;
+  esac
+done
+
+if [[ $install ]]; then
+  npm install && bower install && grunt && grunt connect watch
+fi
