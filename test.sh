5e0e8bf5eeed8bcb766079d295986f3c26a55717
Leon Coto <leoncoto@gmail.com>
2013-08-18 20:53:43 +0100

    Redoing work left at the office.

--- test.sh
+++ test.sh
@@ -12,5 +12,21 @@ while getopts ":i" opt ; do
 done
 
 if [[ $install ]]; then
-  npm install && bower install && grunt && grunt connect watch
+  npm install && bower install &&
+  cd ..
+
+  # Symlink the grunt-carnaby gruntplugin
+  mkdir -p grunt-init-carnaby/node_modules
+  from=$(pwd)'/grunt-carnaby'
+  to=$(pwd)'/a-carnaby-project/node_modules'
+  echo
+  echo 'Symlinking grunt-carnaby gruntplugin...'
+  echo 'From: ' $from
+  echo 'To:   ' $to
+  echo
+  ln -s $from $to
+  ls -laf a-carnaby-project/node_modules
+
+  cd a-carnaby-project
+  grunt server
 fi
