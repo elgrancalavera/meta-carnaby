a182797f4bee81e97c17cf5eda7dd28ad7edf082
Leon Coto <leon.coto@mcsaatchi.com>
2013-09-04 17:05:50 +0100

    basic gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,6 @@
 /*global module:false*/
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
-    // Task configuration.
     jshint: {
       options: {
         curly: true,
@@ -17,38 +14,19 @@ module.exports = function(grunt) {
         unused: true,
         boss: true,
         eqnull: true,
-        globals: {
-          jQuery: true
-        }
+        node: true
       },
       gruntfile: {
         src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
       }
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js']
     },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
         tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
       }
     }
   });
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
-
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.registerTask('default', ['jshint', 'watch']);
 };
